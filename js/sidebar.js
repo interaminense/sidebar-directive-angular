61c4f970ee654458f86eda5353f2f4fa867efce8
interaminense <adriano.interaminense@gmail.com>
2016-12-27 15:45:34 -0200

    add directive sidebar-open

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,45 @@
 var app = angular.module('myApp', []);
 
+app.directive('sidebarOpen', [function() {
+	return {
+		restrict: 'A',
+		link: function (scope, iElement, iAttrs) {
+
+			scope.flag = true;
+			scope.element = iElement[0];
+			scope.body = document.body;
+
+			//control body
+			function controlBody() {
+				if (scope.flag) {
+					scope.body.style.overflowY = "hidden";
+				} else {
+					scope.body.style.overflowY = "auto";
+				}
+
+				scope.flag = !scope.flag;
+			}
+
+			scope.element.addEventListener("click", function() {
+				var _element = document.getElementById(iAttrs.sidebarOpen);
+				_element.classList.toggle("sidebar--is-open");
+
+				var _bgSidebar = document.getElementById(iAttrs.sidebarOpen + "-bg");
+				_bgSidebar.classList.toggle("sidebar-bg--is-visible");
+
+				//control body
+				controlBody();
+			});
+
+		}
+	};
+}]);
+
 app.directive('sidebar', ['$compile', function($compile) {
 	return {
 		restrict: 'E',
 		link: function (scope, iElement, iAttrs) {
 
-			scope.flag = true;
 			scope.body = document.body;
 			scope.element = iElement[0];
 
@@ -17,10 +51,11 @@ app.directive('sidebar', ['$compile', function($compile) {
 				return !isNaN(parseFloat(n)) && isFinite(n);
 			}
 
-			if(isNumber(iAttrs.size))
+			if (isNumber(iAttrs.size)) {
 				scope.element.style.maxWidth = iAttrs.size + 'px';
-			else
+			} else {
 				scope.element.style.maxWidth = iAttrs.size;
+			}
 
 			//add class to position on sidebar
 			scope.element.classList.add("sidebar--" + iAttrs.position);
@@ -29,32 +64,10 @@ app.directive('sidebar', ['$compile', function($compile) {
 			var bgSidebar = document.createElement("div");
 			bgSidebar.setAttribute("class", "sidebar-bg");
 			bgSidebar.setAttribute("id", iAttrs.id + "-bg");
-			bgSidebar.setAttribute("ng-click", "toggleSidebar('"+ iAttrs.id +"')");
+			bgSidebar.setAttribute("sidebar-open", iAttrs.id);
 			scope.body.appendChild(bgSidebar);
 
 			$compile(bgSidebar)(scope);
-
-			//control body
-			var controlBody = function() {
-				if(scope.flag)
-					scope.body.style.overflowY = "hidden";
-				else
-					scope.body.style.overflowY = "auto";
-
-				scope.flag = !scope.flag;
-			}
-
-			//click to show/hide sidebar
-			scope.toggleSidebar = function(idSidebar) {
-				var _element = document.getElementById(idSidebar);
-				_element.classList.toggle("sidebar--is-open");
-
-				var _bgSidebar = document.getElementById(idSidebar + "-bg");
-				_bgSidebar.classList.toggle("sidebar-bg--is-visible");
-
-				//control body
-				controlBody();
-			}
 
 		}
 	};
